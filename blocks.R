2df4ad3bb25edee1405f50be55fc194cd532668d
juanchiem <edwardsmolina@gmail.com>
2021-04-15 12:07:13 -0300

    meteo

---
+++ blocks.R
@@ -0,0 +1,15 @@
+:::{#tip-text .tip}
+  A tip!
+:::
+    
+:::{#note-text .note}
+A note!
+:::
+        
+:::{#important-text .important}
+I have something **important** to say here!
+:::
+            
+:::{.orange-box}
+An orange box!!! Remember figure (\@ref(fig:example-plot))!
+:::
